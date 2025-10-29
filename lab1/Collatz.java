29fb87b36631d588cb6dd800f1c55b61300b4665
Ruben Pulido <77760285+rubenpulido1@users.noreply.github.com>
2025-10-29 12:06:51 -0700

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,25 +3,21 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /* If n is even then return n/2, else if n is odd return 3n+1 */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
+        if (n % 2 == 0) {
+            return n/2;
         }
+        return 3*n + 1;
     }
 
     public static void main(String[] args) {
         int n = 5;
         System.out.print(n + " ");
-        while (n != 1) {
+        while(n != 1) {
             n = nextNumber(n);
             System.out.print(n + " ");
         }
-        System.out.println();
     }
 }
 
